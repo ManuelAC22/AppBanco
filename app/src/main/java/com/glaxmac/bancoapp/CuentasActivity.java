de7c40d6b6629a7661fc9828c030381e6b7e91ec
Cristina Martínez Vásquez <50123596+CristinaMV0997@users.noreply.github.com>
2019-05-08 21:29:10 -0500

    :+1:
    :+1:

--- app/src/main/java/com/glaxmac/bancoapp/CuentasActivity.java
+++ app/src/main/java/com/glaxmac/bancoapp/CuentasActivity.java
@@ -1,5 +1,6 @@
 package com.glaxmac.bancoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,10 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+
 public class CuentasActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button BTNRETIRO, BTNDEPOSITO;
     TextView LBLCUENTA, LBLSALDO;
+
+    String cuenta = "";
+    Double saldo = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +31,33 @@ public class CuentasActivity extends AppCompatActivity implements View.OnClickLi
 
         BTNRETIRO.setOnClickListener(this);
         BTNDEPOSITO.setOnClickListener(this);
+
+        Bundle bundle = getIntent().getExtras();
+        String cuenta2 = bundle.getString("identidificador");
+        saldo = Double.parseDouble(bundle.getString("dataMoney"));
+
+        String[] c = new String[4];
+        c = cuenta2.split("-");
+
+        cuenta = c[0].substring(0,3) + "-" + c[0].substring(3) + c[1]+c[2].substring(0,3) + "-" + c[2].substring(3) + "-" + c[3].substring(0,2);
+        LBLCUENTA.setText(cuenta2);
+        LBLSALDO.setText("S/. "+saldo);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_deposito:{
-
+                Intent intent = new Intent(CuentasActivity.this, DepositoActivity.class);
+                intent.putExtra("identidificador", cuenta);
+                intent.putExtra("dataMoney", saldo);
+                startActivity(intent);
             }break;
             case R.id.btn_retiro:{
-
+                Intent intent = new Intent(CuentasActivity.this, RetiroActivity.class);
+                intent.putExtra("identidificador", cuenta);
+                intent.putExtra("dataMoney", saldo);
+                startActivity(intent);
             }break;
         }
     }
